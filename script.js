a5cb3d95e39089d53b0ccbf4228d59df43f92a1c
Adriano <150024041+Blockymage45@users.noreply.github.com>
2026-03-24 15:23:37 +0000

    variaveis

---
+++ script.js
@@ -0,0 +1,15 @@
+// var
+
+var altura = 5;
+
+var comprimento = 7;
+
+area = altura * comprimento;
+
+console.log(area);
+
+//let
+let forma 'retangulo'
+let altura = 5;
+let comprimento = 7;
+let area;
